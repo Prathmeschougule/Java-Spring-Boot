93e019dda883feb55cf847acda8cee26daaae33e
Prathmesh Chougule <prathmeshuix@gmail.com>
2024-12-05 21:03:44 -0800

    Web Services

--- demo/src/main/java/com/myCompany/demo/DemoApplication.java
+++ demo/src/main/java/com/myCompany/demo/DemoApplication.java
@@ -13,4 +13,7 @@ public class DemoApplication {
 	 	// MyClass.method	();
 	}
 
+
+	
+
 }
--- demo/src/main/java/com/myCompany/demo/MyClass.java
+++ demo/src/main/java/com/myCompany/demo/MyClass.java
@@ -1,7 +1,12 @@
 package com.myCompany.demo;
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 @RestController
 public class MyClass {
@@ -12,6 +17,37 @@ public class MyClass {
         return "<h1>Helo G Kya Hal He </h1>";
     }
         
-    
+
+  
+    @GetMapping("date")
+    public Date myCloack(){
+         Date dt=new Date();
+         return dt;
+    }
+
+    // formeted Date and Time 
+
+    @GetMapping("checkDate")
+    public static String fomatClock(){
+
+        LocalDateTime now=LocalDateTime.now();//current Date And Time 
+        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyy-mm-dd HH:mm:ss") ; //Desired format
+        return now.format(formatter);//format and Retrun As String 
+    }
+
+    //Check Even Oadd Number
+
+    @GetMapping("check")
+    public static String oddEvne(){
+        int number=5;
+
+        if (number % 2 == 0) {
+            return "The Number IS "+number+"Is Even";
+        }else{
+             return "The Number IS "+ number +" The Odd";
+        }
+
+       
+    }
     
 }

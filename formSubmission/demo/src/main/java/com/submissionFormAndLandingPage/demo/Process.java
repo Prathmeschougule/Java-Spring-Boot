8acd95ca4e92fabd6663f2bf99ed2a75e6f2b5d7
Prathmesh Chougule <prathmeshuix@gmail.com>
2024-12-08 09:36:51 -0800

    calsi get  rol send info

--- formSubmission/demo/src/main/java/com/submissionFormAndLandingPage/demo/Process.java
+++ formSubmission/demo/src/main/java/com/submissionFormAndLandingPage/demo/Process.java
@@ -2,7 +2,7 @@ package com.submissionFormAndLandingPage.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 

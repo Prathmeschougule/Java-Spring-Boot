16cc99a4c86d408ad99dd4c387d838eb137100c3
Prathmesh Chougule <prathmeshuix@gmail.com>
2024-12-29 12:09:02 -0800

    Hospital Crud

--- Test DB Practise/practisecrud/practisecrud/src/main/java/com/dbtestsecound_project/practisecrud/User.java
+++ Test DB Practise/practisecrud/practisecrud/src/main/java/com/dbtestsecound_project/practisecrud/User.java
@@ -4,11 +4,15 @@ package com.dbtestsecound_project.practisecrud;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     private   int id;
